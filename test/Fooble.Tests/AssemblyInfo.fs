cb0d2219a701ebd2988fb4a22b7a78f34b5d7226
Lee DeLapp <fooblinator@outlook.com>
2016-04-27 00:22:16 -0400

    Renamed tests project

--- test/Fooble.Tests/AssemblyInfo.fs
+++ test/Fooble.Tests/AssemblyInfo.fs
@@ -1,9 +1,9 @@
-﻿namespace Meta.Core.AssemblyInfo
+﻿namespace Meta.Tests.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.InteropServices
 
-[<assembly:AssemblyTitle("Fooble.Test")>]
+[<assembly:AssemblyTitle("Fooble.Tests")>]
 [<assembly:AssemblyVersion("1.0.0.0")>]
 [<assembly: Guid("f1d4ff61-3982-4605-8baf-1e7f941165cd")>]
 do ()
