db78a25496b9b8cac2ef08b8c7b758c3ebde8588
Lee DeLapp <fooblinator@outlook.com>
2016-05-07 16:33:35 -0400

    Minor edits

--- src/Fooble.Core/AssemblyInfo.fs
+++ src/Fooble.Core/AssemblyInfo.fs
@@ -5,5 +5,5 @@ open System.Runtime.InteropServices
 
 [<assembly:AssemblyTitle("Fooble.Core")>]
 [<assembly:AssemblyVersion("1.0.0.0")>]
-[<assembly: Guid("807e9944-9329-40c3-ac0e-74920a316cb6")>]
+[<assembly:Guid("807e9944-9329-40c3-ac0e-74920a316cb6")>]
 do ()
--- test/Fooble.Tests/AssemblyInfo.fs
+++ test/Fooble.Tests/AssemblyInfo.fs
@@ -5,5 +5,5 @@ open System.Runtime.InteropServices
 
 [<assembly:AssemblyTitle("Fooble.Tests")>]
 [<assembly:AssemblyVersion("1.0.0.0")>]
-[<assembly: Guid("f1d4ff61-3982-4605-8baf-1e7f941165cd")>]
+[<assembly:Guid("f1d4ff61-3982-4605-8baf-1e7f941165cd")>]
 do ()
