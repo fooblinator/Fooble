b3abdedb262650dfdaafc03974669386a388defc
Lee DeLapp <fooblinator@outlook.com>
2016-05-11 20:01:45 -0400

    Added member detail list types and modules

---
+++ src/Fooble.Core/MemberList.fs
@@ -0,0 +1,127 @@
+﻿namespace Fooble.Core
+
+open MediatR
+open System.Runtime.CompilerServices
+
+type IMemberListItemReadModel = 
+    abstract Id : string
+    abstract Name : string
+
+type IMemberListReadModel = 
+    abstract Members : IMemberListItemReadModel seq
+
+type IMemberListQueryFailureStatus = 
+    abstract IsNotFound : bool
+
+type IMemberListQuery = 
+    inherit IRequest<IResult<IMemberListReadModel, IMemberListQueryFailureStatus>>
+
+type IMemberListQueryHandler = IRequestHandler<IMemberListQuery, IResult<IMemberListReadModel, IMemberListQueryFailureStatus>>
+
+[<RequireQualifiedAccess>]
+module internal MemberListItemReadModel = 
+    type private Implementation = 
+        { Id : string
+          Name : string }
+        interface IMemberListItemReadModel with
+            
+            member this.Id = 
+                match this with
+                | { Id = i; Name = _ } -> i
+            
+            member this.Name = 
+                match this with
+                | { Id = _; Name = n } -> n
+    
+    let make id name : IMemberListItemReadModel = 
+        Validation.ensureValid Member.validateId id
+        Validation.ensureValid Member.validateName name
+        { Id = id
+          Name = name } :> _
+
+[<RequireQualifiedAccess>]
+module internal MemberListReadModel = 
+    type private Implementation = 
+        { Members : IMemberListItemReadModel seq }
+        interface IMemberListReadModel with
+            member this.Members = 
+                match this with
+                | { Members = ms } -> ms
+    
+    let validateMembers (members : IMemberListItemReadModel seq) = 
+        if Validation.isNullValue members then 
+            Some(ValidationFailureInfo.make "members" (sprintf "%s should not be null" "Member list"))
+        else if Seq.isEmpty members then 
+            Some(ValidationFailureInfo.make "members" (sprintf "%s should not be empty" "Member list"))
+        else if Validation.containsNullValue members then 
+            Some(ValidationFailureInfo.make "members" (sprintf "%s should not be null" "Member list items"))
+        else None
+    
+    let make members : IMemberListReadModel = 
+        Validation.ensureValid validateMembers members
+        { Members = members } :> _
+
+[<RequireQualifiedAccess>]
+module MemberListQueryFailureStatus = 
+    let (|NotFound|) (status : IMemberListQueryFailureStatus) = ()
+    
+    type private Implementation = 
+        | NotFound
+        interface IMemberListQueryFailureStatus with
+            member this.IsNotFound = 
+                match this with
+                | NotFound -> true
+    
+    [<CompiledName("NotFound")>]
+    let notFound : IMemberListQueryFailureStatus = NotFound :> _
+
+[<RequireQualifiedAccess>]
+module MemberListQuery = 
+    type private Implementation() = 
+        interface IMemberListQuery
+    
+    [<CompiledName("Make")>]
+    let make() : IMemberListQuery = Implementation() :> _
+
+[<RequireQualifiedAccess>]
+module internal MemberListQueryHandler = 
+    let validateQuery query = 
+        if Validation.isNullValue query then 
+            Some(ValidationFailureInfo.make "query" (sprintf "%s should not be null" "Query"))
+        else None
+    
+    type private Implementation(context : IDataContext) = 
+        interface IMemberListQueryHandler with
+            member this.Handle(query) = 
+                Validation.ensureValid validateQuery query
+                match List.ofSeq context.Members with
+                | [] -> Result.failure MemberListQueryFailureStatus.notFound
+                | mds -> 
+                    List.map (fun md -> MemberListItemReadModel.make md.Id md.Name) mds
+                    |> Seq.ofList
+                    |> MemberListReadModel.make
+                    |> Result.success
+    
+    let validateContext context = 
+        if Validation.isNullValue context then 
+            Some(ValidationFailureInfo.make "context" (sprintf "%s should not be null" "Data context"))
+        else None
+    
+    let make context : IMemberListQueryHandler = 
+        Validation.ensureValid validateContext context
+        Implementation(context) :> _
+
+[<Extension>]
+module MemberListExtensions = 
+    [<CompiledName("ToMessageDisplayReadModel"); Extension>]
+    let toMessageDisplayReadModel (result : IResult<IMemberListReadModel, IMemberListQueryFailureStatus>) = 
+        let h = "Member List Query"
+        let ss = MessageDisplaySeverity.informational
+        let sm = "Member list query was successful"
+        let fs = MessageDisplaySeverity.error
+        let fm = "Member list query was not successful and returned not found"
+        match result with
+        | Result.Success _ -> MessageDisplayReadModel.make h ss (Seq.singleton sm)
+        | Result.Failure s -> 
+            match s with
+            | MemberListQueryFailureStatus.NotFound -> MessageDisplayReadModel.make h fs (Seq.singleton fm)
---
+++ test/Fooble.Tests/MemberListTests.fs
@@ -0,0 +1,182 @@
+﻿namespace Fooble.Tests
+
+open Fooble.Core
+open Foq
+open MediatR
+open NUnit.Framework
+open Swensen.Unquote
+open System
+
+[<TestFixture>]
+module MemberListQueryFailureStatusTests = 
+    [<Test>]
+    let ``Calling not found, returns expected status``() = 
+        let status = MemberListQueryFailureStatus.notFound
+        test <@ (box status) :? IMemberListQueryFailureStatus @>
+        test <@ status.IsNotFound @>
+    
+    [<Test>]
+    let ``Calling is not found, with not found status, returns true``() = 
+        let status = MemberListQueryFailureStatus.notFound
+        test <@ status.IsNotFound @>
+
+[<TestFixture>]
+module MemberListItemReadModelTests = 
+    [<Test>]
+    let ``Calling make, with null id, raises expected exception``() = 
+        let expectedParamName = "id"
+        let expectedMessage = "Id should not be null"
+        raisesWith<ArgumentException> <@ MemberListItemReadModel.make null (Helper.randomGuidString()) @> 
+            (fun e -> 
+            <@ e.ParamName = expectedParamName && (Helper.fixArgumentExceptionMessage e.Message) = expectedMessage @>)
+    
+    [<Test>]
+    let ``Calling make, with empty id, raises expected exception``() = 
+        let expectedParamName = "id"
+        let expectedMessage = "Id should not be empty"
+        raisesWith<ArgumentException> <@ MemberListItemReadModel.make "" (Helper.randomGuidString()) @> 
+            (fun e -> 
+            <@ e.ParamName = expectedParamName && (Helper.fixArgumentExceptionMessage e.Message) = expectedMessage @>)
+    
+    [<Test>]
+    let ``Calling make, with invalid guid formatted id, raises expected exception``() = 
+        let expectedParamName = "id"
+        let expectedMessage = "Id should be in the proper GUID format"
+        raisesWith<ArgumentException> 
+            <@ MemberListItemReadModel.make (Helper.randomNonGuidString()) (Helper.randomGuidString()) @> 
+            (fun e -> 
+            <@ e.ParamName = expectedParamName && (Helper.fixArgumentExceptionMessage e.Message) = expectedMessage @>)
+    
+    [<Test>]
+    let ``Calling make, with null name, raises expected exception``() = 
+        let expectedParamName = "name"
+        let expectedMessage = "Name should not be null"
+        raisesWith<ArgumentException> <@ MemberListItemReadModel.make (Helper.randomGuidString()) null @> 
+            (fun e -> 
+            <@ e.ParamName = expectedParamName && (Helper.fixArgumentExceptionMessage e.Message) = expectedMessage @>)
+    
+    [<Test>]
+    let ``Calling make, with empty name, raises expected exception``() = 
+        let expectedParamName = "name"
+        let expectedMessage = "Name should not be empty"
+        raisesWith<ArgumentException> <@ MemberListItemReadModel.make (Helper.randomGuidString()) "" @> 
+            (fun e -> 
+            <@ e.ParamName = expectedParamName && (Helper.fixArgumentExceptionMessage e.Message) = expectedMessage @>)
+    
+    [<Test>]
+    let ``Calling make, with valid parameters, returns expected result``() = 
+        let expectedId = Helper.randomGuidString()
+        let expectedName = Helper.randomGuidString()
+        let readModel = MemberListItemReadModel.make expectedId expectedName
+        test <@ (box readModel) :? IMemberListItemReadModel @>
+        let actualId = readModel.Id
+        test <@ actualId = expectedId @>
+        let actualName = readModel.Name
+        test <@ actualName = expectedName @>
+    
+    [<Test>]
+    let ``Calling id, returns expected id``() = 
+        let expectedId = Helper.randomGuidString()
+        let readModel = MemberListItemReadModel.make expectedId (Helper.randomGuidString())
+        let actualId = readModel.Id
+        test <@ actualId = expectedId @>
+    
+    [<Test>]
+    let ``Calling name, returns expected name``() = 
+        let expectedName = Helper.randomGuidString()
+        let readModel = MemberListItemReadModel.make (Helper.randomGuidString()) expectedName
+        let actualName = readModel.Name
+        test <@ actualName = expectedName @>
+
+[<TestFixture>]
+module MemberListReadModelTests = 
+    [<Test>]
+    let ``Calling make, with null members, raises expected exception``() = 
+        let expectedParamName = "members"
+        let expectedMember = "Member list should not be null"
+        raisesWith<ArgumentException> <@ MemberListReadModel.make null @> 
+            (fun e -> 
+            <@ e.ParamName = expectedParamName && (Helper.fixArgumentExceptionMessage e.Message) = expectedMember @>)
+    
+    [<Test>]
+    let ``Calling make, with empty members, raises expected exception``() = 
+        let expectedParamName = "members"
+        let expectedMember = "Member list should not be empty"
+        raisesWith<ArgumentException> <@ MemberListReadModel.make Seq.empty @> 
+            (fun e -> 
+            <@ e.ParamName = expectedParamName && (Helper.fixArgumentExceptionMessage e.Message) = expectedMember @>)
+    
+    //[<Test>]
+    //let ``Calling make, with not empty members containing null member, raises expected exception``() = 
+    //    let expectedParamName = "members"
+    //    let expectedMember = "Member list items should not be null"
+    //    raisesWith<ArgumentException> <@ MemberListReadModel.make (Seq.singleton null) @> 
+    //        (fun e -> 
+    //        <@ e.ParamName = expectedParamName && (Helper.fixArgumentExceptionMessage e.Message) = expectedMember @>)
+    //    
+    [<Test>]
+    let ``Calling make, with valid parameters, returns expected result``() = 
+        let expectedMembers = [ MemberListItemReadModel.make (Helper.randomGuidString()) (Helper.randomGuidString()) ]
+        let readModel = MemberListReadModel.make (Seq.ofList expectedMembers)
+        test <@ (box readModel) :? IMemberListReadModel @>
+        let actualMembers = List.ofSeq readModel.Members
+        test <@ actualMembers = expectedMembers @>
+    
+    [<Test>]
+    let ``Calling members, returns expected members``() = 
+        let expectedMembers = [ MemberListItemReadModel.make (Helper.randomGuidString()) (Helper.randomGuidString()) ]
+        let readModel = MemberListReadModel.make (Seq.ofList expectedMembers)
+        let actualMembers = List.ofSeq readModel.Members
+        test <@ actualMembers = expectedMembers @>
+
+[<TestFixture>]
+module MemberListQueryTests = 
+    [<Test>]
+    let ``Calling make, with valid parameters, returns expected result``() = 
+        let query = MemberListQuery.make()
+        test <@ (box query) :? IMemberListQuery @>
+
+[<TestFixture>]
+module MemberListQueryHandlerTests = 
+    [<Test>]
+    let ``Calling make, with null context, raises expected exception``() = 
+        let expectedParamName = "context"
+        let expectedMessage = "Data context should not be null"
+        raisesWith<ArgumentException> <@ MemberListQueryHandler.make null @> 
+            (fun e -> 
+            <@ e.ParamName = expectedParamName && (Helper.fixArgumentExceptionMessage e.Message) = expectedMessage @>)
+    
+    [<Test>]
+    let ``Calling make, with valid parameters, returns expected result``() = 
+        let handler = Mock<IDataContext>().Create() |> MemberListQueryHandler.make
+        test <@ (box handler) :? IMemberListQueryHandler @>
+
+//    [<Test>]
+//    let ``Calling handle, with null query, raises expected exception``() = 
+//        let expectedParamName = "query"
+//        let expectedMessage = "Query should not be null"
+//        let handler = Mock<IDataContext>().Create() |> MemberListQueryHandler.make
+//        raisesWith<ArgumentException> <@ handler.Handle(null) @> 
+//            (fun e -> 
+//            <@ e.ParamName = expectedParamName && (Helper.fixArgumentExceptionMessage e.Message) = expectedMessage @>)
+//    
+[<TestFixture>]
+module MemberListExtensionTests = 
+    [<Test>]
+    let ``Calling to message display read model, with success result, returns expected result``() = 
+        let expectedHeading = "Member List Query"
+        let expectedSeverity = MessageDisplaySeverity.informational
+        let expectedMessages = [ "Member list query was successful" ]
+        
+        let readModel = 
+            [ MemberListItemReadModel.make (Helper.randomGuidString()) (Helper.randomGuidString()) ]
+            |> MemberListReadModel.make
+            |> Result.success
+            |> MemberListExtensions.toMessageDisplayReadModel
+        
+        let actualHeading = readModel.Heading
+        test <@ actualHeading = expectedHeading @>
+        let actualSeverity = readModel.Severity
+        test <@ actualSeverity = expectedSeverity @>
+        let actualMessages = List.ofSeq readModel.Messages
+        test <@ actualMessages = expectedMessages @>
