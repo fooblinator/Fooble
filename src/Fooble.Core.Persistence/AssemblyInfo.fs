569841978c9dee28b948fac399e025310a88f696
Lee DeLapp <fooblinator@outlook.com>
2016-05-23 15:20:43 -0400

    Placed persistence related code in separate module

--- src/Fooble.Core.Persistence/AssemblyInfo.fs
+++ src/Fooble.Core.Persistence/AssemblyInfo.fs
@@ -4,9 +4,10 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-[<assembly:AssemblyTitle("Fooble.Core")>]
+[<assembly:AssemblyTitle("Fooble.Core.Persistence")>]
 [<assembly:AssemblyVersion("1.0.0.0")>]
-[<assembly:Guid("807e9944-9329-40c3-ac0e-74920a316cb6")>]
+[<assembly:Guid("6324603a-6972-4b7e-ae8b-a5e4a13a657f")>]
+[<assembly:InternalsVisibleTo("Fooble.Core")>]
 [<assembly:InternalsVisibleTo("Fooble.Tests")>]
 [<assembly:InternalsVisibleTo("DynamicProxyGenAssembly2")>]
 do ()
---
+++ src/Fooble.Core.Persistence/Persistence.fs
@@ -0,0 +1,33 @@
+﻿namespace Fooble.Core.Persistence
+
+open FSharp.Data.TypeProviders
+open System
+open System.Data.Objects
+
+(* Persistence *)
+
+type internal EntityConnection = SqlEntityConnection<ConnectionStringName = "FoobleContext">
+type internal FoobleContext = EntityConnection.ServiceTypes.EntityContainer
+type internal MemberData = EntityConnection.ServiceTypes.MemberData
+
+[<AllowNullLiteral>]
+type internal IFoobleContext =
+    inherit IDisposable
+    abstract MemberData : IObjectSet<MemberData>
+    abstract SaveChanges : unit -> int
+
+[<AutoOpen>]
+module internal Helpers =
+
+    (* Helpers *)
+
+    let internal makeFoobleContext connectionString =
+        let context =
+            match connectionString with
+            | Some x -> EntityConnection.GetDataContext(x).DataContext :?> FoobleContext
+            | None -> EntityConnection.GetDataContext().DataContext :?> FoobleContext
+
+        { new IFoobleContext with
+              member this.Dispose() = context.Dispose()
+              member this.MemberData = context.MemberData :> IObjectSet<MemberData>
+              member this.SaveChanges() = context.SaveChanges() }
